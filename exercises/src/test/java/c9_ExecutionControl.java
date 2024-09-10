e885195dac35b1ec0d68e4da9f972b72d28ee28c
ykoreyba <Yuriy.Koreyba@openbet.com>
2024-09-10 09:22:04 +0300

    chapter 9, ex 7

--- exercises/src/test/java/c9_ExecutionControl.java
+++ exercises/src/test/java/c9_ExecutionControl.java
@@ -177,10 +177,13 @@ public class c9_ExecutionControl extends ExecutionControlBase {
     public void event_processor() {
         //todo: feel free to change code as you need
         Flux<String> eventStream = eventProcessor()
-                .filter(event -> event.metaData.length() > 0)
-                .doOnNext(event -> System.out.println("Mapping event: " + event.metaData))
-                .map(this::toJson)
-                .concatMap(n -> appendToStore(n).thenReturn(n));
+          .parallel()
+          .runOn(Schedulers.parallel())
+          .filter(event -> event.metaData.length() > 0)
+          .doOnNext(event -> System.out.println("Mapping event: " + event.metaData))
+          .map(this::toJson)
+          .sequential()
+          .concatMap(n -> appendToStore(n).thenReturn(n));
 
         //don't change code below
         StepVerifier.create(eventStream)
